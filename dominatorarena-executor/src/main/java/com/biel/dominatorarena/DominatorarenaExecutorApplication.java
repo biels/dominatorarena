a668761fa2294863547d5b48529f27ac853701f1
Biel Simon Rojas <bielsimon2@gmail.com>
2016-12-05 10:50:29 +0100

    Added executor

---
+++ dominatorarena-executor/src/main/java/com/biel/dominatorarena/DominatorarenaExecutorApplication.java
@@ -0,0 +1,12 @@
+package com.biel.dominatorarena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DominatorarenaExecutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DominatorarenaExecutorApplication.class, args);
+	}
+}
---
+++ dominatorarena-executor/src/main/java/com/biel/dominatorarena/Worker.java
@@ -0,0 +1,12 @@
+package com.biel.dominatorarena;
+
+import com.biel.dominatorarena.api.requests.RegisterWorkerRequest;
+
+/**
+ * Created by Biel on 1/12/2016.
+ */
+public class Worker {
+    public void g(){
+        new RegisterWorkerRequest();
+    }
+}
