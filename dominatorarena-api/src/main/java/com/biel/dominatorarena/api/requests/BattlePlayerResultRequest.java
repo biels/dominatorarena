cd079d2c09580adc564c4538505053254d3ea336
Biel Simon Rojas <bielsimon2@gmail.com>
2017-05-08 18:59:05 +0200

    Completed BattlePlayerResultRequest.java

--- dominatorarena-api/src/main/java/com/biel/dominatorarena/api/requests/BattlePlayerResultRequest.java
+++ dominatorarena-api/src/main/java/com/biel/dominatorarena/api/requests/BattlePlayerResultRequest.java
@@ -4,6 +4,27 @@ package com.biel.dominatorarena.api.requests;
  * Created by Biel on 8/5/2017.
  */
 public class BattlePlayerResultRequest {
-    int score;
-    int cpuTime;
+    private int score;
+    private int cpuTime;
+
+    public BattlePlayerResultRequest(int score, int cpuTime) {
+        this.score = score;
+        this.cpuTime = cpuTime;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getCpuTime() {
+        return cpuTime;
+    }
+
+    public void setCpuTime(int cpuTime) {
+        this.cpuTime = cpuTime;
+    }
 }
